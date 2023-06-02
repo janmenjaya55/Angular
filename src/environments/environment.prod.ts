8649b85e1fc448eda21f3173d2ea1c1e680939c6
janmenjaya55 <119396869+janmenjaya55@users.noreply.github.com>
2023-06-02 18:01:10 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   environmentName: 'prod',
-    apiUrl: 'http://192.168.56.13',
-  apiUrlone: 'http://192.168.56.13',
-  apiUrltwo: 'http://192.168.56.13',
+    apiUrl: 'http://192.168.56.11',
+  apiUrlone: 'http://192.168.56.11',
+  apiUrltwo: 'http://192.168.56.11',
   
-  apiUrlthree: 'http://192.168.56.13'
+  apiUrlthree: 'http://192.168.56.11'
 
 
 };
