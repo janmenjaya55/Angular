8a4f0130776f09ceb2d03ba9c6fc5e4543e92233
janmenjaya55 <119396869+janmenjaya55@users.noreply.github.com>
2023-07-20 16:53:19 +0530

    Update api-urlservice.service.ts

--- src/app/apiurl/api-urlservice.service.ts
+++ src/app/apiurl/api-urlservice.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 //import { environment } from 'src/environments/environment';
 import { environment} from 'src/environments/environment.prod';
  
@@ -27,7 +29,7 @@ export class ApiUrlserviceService {
   environmentUrltwo= '';
   environmentUrlthree= '';
   apiURL: string;
-  constructor() { 
+  constructor(private _http: HttpClient) { 
 
     this.environmentName = environment.environmentName;
     this.environmentUrl = environment.apiUrl;
@@ -53,11 +55,17 @@ export class ApiUrlserviceService {
         //this.baseURL1 = this.environmentUrlone + ':5011/'
        
       }else {
-        this.baseURL = this.environmentUrl + ':8091/bataloginzull/'
-        this.baseURL1 = this.environmentUrlone + ':8091/batazull/'
-        this.baseURL2 = this.environmentUrltwo + ':8091/batadoczull/'
-        this.baseURL3 = this.environmentUrlthree + ':8091/batamediazull/'
-        this.baseURL4 = this.environmentUrlthree + ':8091/bataauthzull/'
+        this.baseURL = this.environmentUrl + ':30002/bataloginzull/'
+        this.baseURL1 = this.environmentUrlone + ':30002/batazull/'
+        this.baseURL2 = this.environmentUrltwo + ':30002/batadoczull/'
+        this.baseURL3 = this.environmentUrlthree + ':30002/batamediazull/'
+        this.baseURL4 = this.environmentUrlthree + ':30002/bataauthzull/'
+
+        //this.baseURL = this.environmentUrl + ':8091/bataloginzull/'
+        //this.baseURL1 = this.environmentUrlone + ':8091/batazull/'
+        //this.baseURL2 = this.environmentUrltwo + ':8091/batadoczull/'
+        //this.baseURL3 = this.environmentUrlthree + ':8091/batamediazull/'
+        //this.baseURL4 = this.environmentUrlthree + ':8091/bataauthzull/'
         console.log("local"+'environmentUrl: '+this.baseURL)
         console.log("local"+'environmentUrl: '+this.baseURL1)
         console.log("local"+'environmentUrl: '+this.baseURL2)
@@ -75,5 +83,105 @@ export class ApiUrlserviceService {
       console.log("local"+'environmentUrl: '+this.baseURL2)
       console.log("local"+'environmentUrljjjjjjjjjjjjjjjjjjjj: '+this.baseURL3)
 
+  }
+
+  //httpStr: string = window.location.href.substring(0, window.location.href.indexOf(':'));
+ 
+
+  securepost(url: string, data: any) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      //_hd = _hd.set("BATA_USER_SIGNATURE", saTkn);
+      _hd = _hd.set('Content-Type', 'application/json');
+      _hd = _hd.set("Authorization", "Bearer "+saTkn);
+      return this._http.post( url, data, { headers: _hd });
+    } else {
+      return new Observable<any>();
+    }
+
+  }
+  secureput(url: string, data: any) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    console.log(saTkn);
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      _hd = _hd.set("Authorization", saTkn);
+      return this._http.put( url, data, { headers: _hd });
+    } else {
+      return new Observable<any>();
+    }
+  }
+
+  secureget(url: string) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      _hd = _hd.set('Accept', 'application/json');
+      _hd = _hd.set('Content-Type', 'application/json');
+      _hd = _hd.set("Authorization", "Bearer "+saTkn);
+      console.log("@@@@@@@@@@@@@@@@@@@@" + url);
+      return this._http.get(  url, { headers: _hd });
+    } else {
+      return new Observable<any>();
+    }
+  }
+
+  securedelete(url: string) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      _hd = _hd.set("Authorization", saTkn);
+      console.log(saTkn);
+      return this._http.delete( url, { headers: _hd });
+    } else {
+      return new Observable<any>();
+    }
+  }
+
+
+  secureMultipart(url: string, data: any) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      _hd = _hd.set("Authorization", saTkn);
+      return this._http.post(url, data, { headers: _hd });
+    } else {
+      return new Observable<any>();
+    }
+
+  }
+
+  get(url: string) {
+    return this._http.get( url);
+
+  }
+
+  post(url: string, data: any) {
+
+    return this._http.post(url, data);
+
+  }
+
+  delete(url: string) {
+    return this._http.delete( url)
+  }
+
+
+  put(url: string, data: any) {
+    return this._http.put( url, data)
+  }
+
+  securegetBlob(url: string) {
+    let saTkn = sessionStorage.getItem("S_A_Token");
+    if (saTkn) {
+      let _hd = new HttpHeaders();
+      _hd = _hd.set('Accept', 'application/octet-stream');
+      _hd = _hd.set('Content-Type', 'application/json');
+      _hd = _hd.set("Authorization", "Bearer "+saTkn);
+      return this._http.get(url, { headers: _hd,responseType: 'blob' });
+    } else {
+      return new Observable<any>();
+    }
   }
 }
